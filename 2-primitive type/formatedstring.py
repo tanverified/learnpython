a716e17da4bcad3c974163c72e9dfabd93467901
tanverified <khantanveer2u@gmail.com>
2021-03-16 18:17:37 +0530

    string methods

---
+++ 2-primitive type/formatedstring.py
@@ -0,0 +1,11 @@
+first = "Ninja"
+last = "Mario"
+
+# full = first + " " + last
+
+full = f"{first} {last}"
+
+print(full)
+
+
+# can use F  or  f  for formated string
---
+++ 2-primitive type/stringMethos.py
@@ -0,0 +1,10 @@
+course = "   python programming  "
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())  # rstrip lstrip
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("ruby" not in course)
