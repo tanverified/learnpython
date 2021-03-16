c53783aad5bbcd8c45bef4203523c5b8418e6dc5
tanverified <khantanveer2u@gmail.com>
2021-03-16 17:21:41 +0530

    escape

---
+++ 2-primitive type/string.py
@@ -0,0 +1,20 @@
+course = "Python Programming"
+
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:5])
+print(course[:])
+
+# can use triple quotes for formated string like
+message = """
+  Hello there
+
+  I am learning python programming.
+  blah blah bla
+
+  Thanks you 
+  Tan
+"""
