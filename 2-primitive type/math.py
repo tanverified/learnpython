f75471e4aacb37309c70abb2d9ba35827ba30527
tanverified <khantanveer2u@gmail.com>
2021-03-17 09:53:44 +0530

    math module

---
+++ 2-primitive type/math.py
@@ -0,0 +1,13 @@
+import math
+
+print(round(2.9))
+print(abs(-2.2))
+
+print(math.ceil(2.23))
+print(math.floor(5.7))
+print(math.pi)
+
+print(math.nan)
+
+# There are many math function available at
+# https://docs.python.org/3/library/math.html
