476b74c57f0727db2ada83bc9aaffa4f215ea7c0
tanverified <khantanveer2u@gmail.com>
2021-03-17 10:39:41 +0530

    type conversion

---
+++ 2-primitive type/typeconversion.py
@@ -0,0 +1,17 @@
+x = input("x: ")
+# print(type(x))
+
+y = int(x) + 1
+
+print(f"x is {x} and y is {y}")
+
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+# Falsy
+# 0
+# ""
+# None
+# Everything else is Truthy
