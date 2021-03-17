225650199972c8cfd545789a4b025d785984be4e
tanverified <khantanveer2u@gmail.com>
2021-03-17 09:07:54 +0530

    numbers in python

---
+++ 2-primitive type/Numbers.py
@@ -0,0 +1,16 @@
+# x = 1
+# x = 1.1
+# x = 1 + 2j   #a + bi
+
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+x += 3
