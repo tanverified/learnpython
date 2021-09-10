726cae73b86cea8034416360f7b0f849c5baa271
tanverified <khantanveer2u@gmail.com>
2021-09-10 09:57:33 +0530

    magic,comparing,arithimatic

---
+++ 7-Classes/8-arthimaticOperator.py
@@ -0,0 +1,15 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
+point = Point(1, 2)
+other = Point(3, 4)
+
+combined = point + other
+
+print(combined.x,combined.y)
